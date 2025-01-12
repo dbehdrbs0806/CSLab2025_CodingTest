343345f20caf9b243fadd650da6af4ed667331a2
ojing133 <erin3118@naver.com>
2025-01-12 20:49:36 +0900

    [Bj-0112] 2644 success

--- eojin/0107_2644.py
+++ eojin/0107_2644.py
@@ -38,41 +38,53 @@ Baekjoon #2644 촌수계산
     - 다시 굴려봄) 큐에 사람하고 거리 정보를 둘 다 저장해서 계산... < 이거 된다
         - 구현하면서 틀렸던 거
             - 인덱스( 찾아보니 0-based / 1-based를 혼용해서 써서 그렇다고 함... 앞으로는 입력 받을 때부터 0-based로 고쳐 받는 게 좋을듯)
-            - python 문법상 != 보다는 is not이나 not을 쓰는 게 좀 더 익숙해지면 좋을듯 (가독성이 좀 더 좋아진다)
+
+- 좀 더 깔끔하게 고칠 순 없나...
+    - python 문법에서 !=랑 is not은 용례상 차이가 있는 듯 함
+        - !=는 값만 비교 / is not은 객체가 같은 메모리를 참조하는지 비교(객체 참조 비교) => 그래서 None의 경우에는 is not None 사용
+    - None은 0이나 False랑 같이 취급되는 듯? 조건식을 좀 더 알아보자
+        - python 문법상 None / 0 / False / 이외 공백문자열은 같게 취급함 => None이 아닌 경우만을 필요로 하면 is not None을 쓰는 게 정확하긴 하다
+    - enumerate() 라는 함수가 있음! 이 경우 인덱스와 값을 동시에 활용 가능, 이 부분 활용해서 코드 고치면 좀 더 가독성 올라갈듯
+    - python은 ! 연산자를 사용하지 않음! 대신 not 키워드를 사용한다
+    - 지금 인덱스 참조가 1-based index랑 0-based index를 혼용해서 헷갈리는 부분이 좀 있음 => 1-based index로 입력받은 값을 0-based index로 처음부터 맞춰주는 게 좋을듯?
+        - 근데 이러면 0으로 if문이 false 판정 날 경우를 대비해야 할듯
+    - if not (visited[parent[curr-1]-1] if parent[curr-1] else True):는 너무 가독성이 떨어지고 지저분함 => parent 값의 None 여부를 먼저 검사하면 깔끔
+    - return을 통해 깔끔하게 종료할 수 있게 다음부터는 함수로 만들어보기
 """
 from collections import deque
 
 allp = int(input()) # 전체 사람 수
 a, b = map(int, input().split()) # 촌수를 계산해야 하는 각각의 사람 번호
 n = int(input()) # 부모 자식들 간의 관계의 개수
-parent = [None] * allp
+parent = [None] * allp # parent 배열(=어떤 사람(인덱스)에 대한 부모 값을 갖는 배열)을 None으로 초기화
 
+# 관계 입력받아 parent에 저장
 for _ in range(n):
     p, c = map(int, input().split())
     parent[c-1] = p
 
-deq = deque([(a, 0)])
-visited = [False] * allp
-visited[a-1] = True
-length = None
+deq = deque([(a, 0)]) # 덱 생성 후 (출발점, dist(거리)) 튜플 추가
+visited = [False] * allp # 방문한 노드 표시 배열 False로 초기화
+visited[a-1] = True # 첫번째 노드를 방문 표시
+length = None # 최종 거리 저장 겸 플래그용 변수
 
-while deq:
+while deq: # 덱이 빌 때까지 반복
     curr, dist = deq.popleft()
     # 부모 검사
-    if not (visited[parent[curr-1]-1] if parent[curr-1] is not None else True):
-        if parent[curr-1] == b:
+    if parent[curr-1] and not visited[parent[curr-1]-1]: # parent 값이 None일 경우 오류 회피 위해 parent 값을 먼저 검사하도록 함
+        if parent[curr-1] == b: # 부모 노드가 도착점과 일치할 경우 탈출
             length = dist + 1
             break
-        deq.append((parent[curr-1], dist+1))
-        visited[parent[curr-1]-1] = True
+        deq.append((parent[curr-1], dist+1)) # 거리를 1 추가하여 덱에 추가
+        visited[parent[curr-1]-1] = True # 방문하였음을 체크
     # 자식 검사
-    for i in range(allp):
-        if parent[i] == curr and (not visited[i]):
-            if i == b - 1:
+    for i, p in enumerate(parent): # enumerate() 함수를 사용하여 for문에서 인덱스와 값 둘 다 사용
+        if p == curr and not visited[i]: # curr의 자식이고 방문하지 않은 노드
+            if i == b-1: # 자식 노드 중 도착점과 일치하는 노드가 있으면 탈출
                 length = dist + 1
                 break
-            deq.append((i+1, dist+1))
-            visited[i] = True
-    if length is not None: break
+            deq.append((i+1, dist+1)) # 거리를 1 추가하여 덱에 추가
+            visited[i] = True # 방문 여부 체크
+    if length is not None: break # length가 None이 아니게 되면 도착점을 찾은 것이므로 루프 탈출(for문 탈출 후 while문 종료용)
 
 print(length if length is not None else -1)
